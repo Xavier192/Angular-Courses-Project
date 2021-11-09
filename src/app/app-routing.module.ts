1157156ae5ea1e3c7e9d64e3fb79d61235c670ff
Xavier <xavier01242@gmail.com>
2021-11-09 21:42:10 +0100

    Defined routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/dashboard', pathMatch:'full'},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'courses',component:CourseListComponent},
+  {path:'detail/:id',component:CourseDetailsComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
