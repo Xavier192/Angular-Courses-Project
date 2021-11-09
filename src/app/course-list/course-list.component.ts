db3c860a36ed2ca910f43abc2300370103d3fbc8
Xavier <xavier01242@gmail.com>
2021-11-09 19:35:12 +0100

    Injected course list with course service

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,4 +1,6 @@
+import { CourseService } from './../course.service';
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../course';
 
 @Component({
   selector: 'app-course-list',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
+  courses:Course[] = [];
 
-  constructor() { }
+  constructor(private CourseService: CourseService) { }
 
   ngOnInit(): void {
+  }
+
+  getCourses(){
+    this.CourseService.getCourses().subscribe((courses:Course[]) => this.courses = courses);
   }
 
 }
